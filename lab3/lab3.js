318db1b9e694f2bd481f586032b8f635a5a6707a
nazar-bb <nazar.babuch23@gmail.com>
2026-03-24 14:41:35 +0200

    task1.2

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -9,3 +9,32 @@ let symb = Symbol("id");
 let obj = { name: "Nazar", age: 18 };
 let mas = [1, 2, 4];
 let undef;
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof bull);
+console.log(typeof nul);
+console.log(typeof bigNumber);
+console.log(typeof symb);
+console.log(typeof obj);
+console.log(typeof mas);
+console.log(typeof undef);
+
+// комент
+
+/*
+1
+2
+3
+*/
+
+number += 5;
+console.log(number);
+number -= 3;
+console.log(number);
+number *= 2;
+console.log(number);
+number /= 4;
+console.log(number);
+number %= 2;
+console.log(number);
