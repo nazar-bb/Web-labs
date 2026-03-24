3a23d7b3cd2758b93f7543557e318307250b73ba
nazar-bb <nazar.babuch23@gmail.com>
2026-03-24 14:34:13 +0200

    task1

---
+++ lab3/lab3.js
@@ -0,0 +1,11 @@
+console.log ("Hello")
+
+let number = 10;
+let string = "Nazar";
+let bull = true;
+let nul = null;
+let bigNumber = 12345678987654321n;
+let symb = Symbol("id");
+let obj = { name: "Nazar", age: 18 };
+let mas = [1, 2, 4];
+let undef;
